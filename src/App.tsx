effd22ec9ca5b41f71bad20fa2b885679fced256
Danang Nur <danang.arbansa@personio.de>
2020-04-11 23:24:30 +0200

    define machine

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import { createMachine } from 'xstate';
+
+const machine = createMachine({
+    initial: 'idle',
+    context: {
+        filter: null,
+        dogBreeds : [],
+        dogImage: null,
+    },
+    states: {
+        idle: {
+            on: {
+                FETCH: 'loading'
+            }
+        },
+        loading: {
+            on: {
+                SUCCESS: 'idle',
+                ERROR: 'error'
+            }
+        },
+        error: {
+            on: {
+                RETRY: 'loading',
+            }
+        }
+    }
+});
 
 export default function App() {
-    return <div>This is App</div>
+
+    return <div></div>
 }
